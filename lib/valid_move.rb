9235b86509154657898c1a6518b6aac8104d0373
Stephane <son.trinh.dev@gmail.com>
2017-12-23 04:26:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,6 +8,6 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   blanks = [' ', '', nil]
-  blanks.include?(board[index])
+  blanks.include?(board[index]) ? false: true
   
 end
